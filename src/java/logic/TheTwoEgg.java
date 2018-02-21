0769a8769a44cc055b0ae5f50c45840f783ec037
Danuway <daniel@almaz@yahoo.com>
2018-02-21 09:09:09 +0100

    adjust project structure

---
+++ src/java/logic/TheTwoEgg.java
@@ -0,0 +1,41 @@
+package java.logic;
+
+import java.util.ArrayList;
+
+public class TheTwoEgg {
+
+	/**
+	 * A building has 100 floors. One of the floors is the highest
+	 * floor an egg can be dropped from without breaking.
+	 * If an egg is dropped from above that floor, it will break.
+	 * If it is dropped from that floor or below, 
+	 * it will be completely undamaged and you can drop the egg again.
+	 * Given two eggs, find the highest floor an egg can be 
+	 * dropped from without breaking, with as few drops as possible. 
+	 * 
+	 * 
+	 */
+	
+	
+	private int sentinal;
+	private ArrayList<Integer> floors;
+	
+	
+	
+	public TheTwoEgg(int sentinal, ArrayList<Integer> floors) {
+		this.sentinal = sentinal;
+		this.floors = floors;
+	}
+	
+	
+	
+	public int getSentinal() {
+		return sentinal;
+	}
+	public ArrayList<Integer> getFloors() {
+		return floors;
+	}
+	
+	
+	
+}
